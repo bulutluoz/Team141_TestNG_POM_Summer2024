025a2c68494fce0a0cbe2c0780ed2b15c1fcc3a0
bulutluoz <abbulutluoz@gmail.com>
2024-06-01 16:32:15 +0200

    19.gun sonu

---
+++ src/test/java/tests/day19_testNGFramework_assertions/C01_TestDatasiKullanimi.java
@@ -0,0 +1,33 @@
+package tests.day19_testNGFramework_assertions;
+
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.TestotomasyonuPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class C01_TestDatasiKullanimi {
+
+    @Test
+    public void aramaTesti(){
+
+        // testotomasyonu anasayfaya gidin
+        Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
+        ReusableMethods.bekle(3);
+
+        // phone icin arama yapin
+        TestotomasyonuPage testotomasyonuPage = new TestotomasyonuPage();
+        testotomasyonuPage.aramakutusu.sendKeys(ConfigReader.getProperty("toAranacakKelime") + Keys.ENTER);
+
+        // arama sonucunda urun bulunabildigini test edin
+        ReusableMethods.bekle(3);
+        Assert.assertTrue(  testotomasyonuPage.sonucElementleriList.size() > 0);
+
+        // sayfayi kapatin
+        Driver.quitDriver();
+
+
+    }
+}
