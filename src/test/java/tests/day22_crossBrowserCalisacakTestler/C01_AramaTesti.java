fae1b5dfd75efde94f62a2d1d3612a775e796ae8
bulutluoz <abbulutluoz@gmail.com>
2024-06-06 20:49:34 +0200

    22.gun sonu

---
+++ src/test/java/tests/day22_crossBrowserCalisacakTestler/C01_AramaTesti.java
@@ -0,0 +1,21 @@
+package tests.day22_crossBrowserCalisacakTestler;
+
+import org.testng.annotations.Test;
+
+public class C01_AramaTesti {
+
+
+    @Test
+    public void aramaTesti(){
+
+        // testotomasyonu anasayfaya gidin
+
+        // belirlenmis aranacak kelime icin arama yapin
+
+        // arama sonucunda urun bulunabildigini test edin
+
+        // sayfayi kapatin
+
+
+    }
+}
---
+++ src/test/java/tests/day22_crossBrowserCalisacakTestler/C02_PozitifLoginTesti.java
@@ -0,0 +1,24 @@
+package tests.day22_crossBrowserCalisacakTestler;
+
+import org.testng.annotations.Test;
+
+public class C02_PozitifLoginTesti {
+
+    @Test
+    public void pozitifLoginTesti(){
+
+        //1- https://www.testotomasyonu.com/ anasayfasina gidin
+
+        //2- account linkine basin
+
+        //3- Kullanici email'i olarak gecerli email girin
+
+        //4- Kullanici sifresi olarak gecerli password girin
+
+        //5- Login butonuna basarak login olun
+
+        //6- Basarili olarak giris yapilabildigini test edin
+
+        //7-sayfayi kapatin
+    }
+}
