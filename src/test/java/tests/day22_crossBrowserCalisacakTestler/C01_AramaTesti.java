839b4c38805a291a98af2590af5789d7aa2c0323
bulutluoz <abbulutluoz@gmail.com>
2024-06-06 21:15:18 +0200

    22.gun sonu

--- src/test/java/tests/day22_crossBrowserCalisacakTestler/C01_AramaTesti.java
+++ src/test/java/tests/day22_crossBrowserCalisacakTestler/C01_AramaTesti.java
@@ -1,21 +1,43 @@
 package tests.day22_crossBrowserCalisacakTestler;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.TestBaseCross;
 
-public class C01_AramaTesti {
+public class C01_AramaTesti extends TestBaseCross {
 
 
     @Test
     public void aramaTesti(){
 
         // testotomasyonu anasayfaya gidin
+        driver.get(ConfigReader.getProperty("toUrl"));
 
         // belirlenmis aranacak kelime icin arama yapin
 
+        // POM'de locate'leri Page class'larindan aliriz
+        // Ancak page class'larinda Driver Class'indaki getDriver() ile olusturulan WebDriver objesi tanimli
+        // Eger cross browser calistiracagimiz class'lar fazla ise
+        // driver'i tanimlayacagimiz yeni Page class'i olusturup, locate'leri oradan kullanabiliriz
+        // bizim framework'umuzde CrossBrowser calisacak testler sinirli sayida oldugundan
+        // yeni bir Page class'i olusturmak yerine
+        // locate'leri eski usul ile @Test method'u icinde yapabiliriz
+
+        WebElement aramaKutusu = driver.findElement(By.id("global-search"));
+        aramaKutusu.sendKeys(ConfigReader.getProperty("toAranacakKelime") + Keys.ENTER);
+
+
         // arama sonucunda urun bulunabildigini test edin
+        WebElement aramaSonucElementi = driver.findElement(By.className("product-count-text"));
 
-        // sayfayi kapatin
+        String unExpectedSonuc = "0 Products Found";
+        String actualSonuc = aramaSonucElementi.getText();
 
+        Assert.assertNotEquals(actualSonuc,unExpectedSonuc);
 
     }
 }
--- src/test/java/tests/day22_crossBrowserCalisacakTestler/C02_PozitifLoginTesti.java
+++ src/test/java/tests/day22_crossBrowserCalisacakTestler/C02_PozitifLoginTesti.java
@@ -1,24 +1,41 @@
 package tests.day22_crossBrowserCalisacakTestler;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.TestBaseCross;
 
-public class C02_PozitifLoginTesti {
+public class C02_PozitifLoginTesti extends TestBaseCross {
 
     @Test
     public void pozitifLoginTesti(){
 
         //1- https://www.testotomasyonu.com/ anasayfasina gidin
+        driver.get(ConfigReader.getProperty("toUrl"));
 
         //2- account linkine basin
+        driver.findElement(By.xpath("//span[text()='Account']"))
+                .click();
 
         //3- Kullanici email'i olarak gecerli email girin
+        WebElement emailKutusu = driver.findElement(By.id("email"));
+        emailKutusu.sendKeys(ConfigReader.getProperty("toGecerliEmail"));
 
         //4- Kullanici sifresi olarak gecerli password girin
+        WebElement passwordKutusu = driver.findElement(By.id("password"));
+        passwordKutusu.sendKeys(ConfigReader.getProperty("toGecerliPassword"));
 
         //5- Login butonuna basarak login olun
+        driver.findElement(By.id("submitlogin"))
+                .click();
 
         //6- Basarili olarak giris yapilabildigini test edin
+        WebElement logoutButonu = driver.findElement(By.xpath("//span[text()='Logout']"));
 
-        //7-sayfayi kapatin
+        Assert.assertTrue(logoutButonu.isDisplayed());
+
+
     }
 }
